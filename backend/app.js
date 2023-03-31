0963a870cf6ce2492b61d52e4884cfc13cd6a6ad
Daniel Guenther <danthemango@gmail.com>
2023-03-30 23:44:20 -0700

    updating swagger UI to the backend from a tutorial

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,46 @@
-const express = require('express')
-const PORT = 3000
-const HOST = '0.0.0.0'
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var { initialize } = require("express-openapi");
+var swaggerUi = require("swagger-ui-express");
 
-// App
-const app = express()
+var app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World.')
 });
 
-app.listen(PORT, HOST)
-console.log(`Our app running on http://${HOST}:${PORT}`)
+app.listen(3030);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// enable CORS on all routes for now
+app.use(cors());
+
+// OpenAPI routes
+initialize({
+  app,
+  apiDoc: require("./api/api-doc"),
+  paths: "./api/paths",
+});
+
+// OpenAPI UI
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(null, {
+    swaggerOptions: {
+      url: "http://localhost:3030/api-docs",
+    },
+  })
+);
+
+console.log("App running on port http://localhost:3030");
+console.log(
+  "OpenAPI documentation available in http://localhost:3030/api-documentation"
+);
+
+module.exports = app;
