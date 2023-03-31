64c923f8290f1566da80b8fb714b620120a21dd5
Daniel Guenther <danthemango@gmail.com>
2023-03-31 00:34:32 -0700

    moved helper script and added new one
    - the new helper script converts the static product array into an object
      with entries specified by ID. It won't make much of a difference in
      terms of access speed but it should simulate access by unique entry
      in a table or what have you
    - move scripts out of frontend since they're only temporarily useful
      probably and project agnostic

---
+++ scripts/productArr2obj.js
@@ -0,0 +1,15 @@
+// convert json array into an object with keys
+// specified by projectId
+// usage: node productArr2obj.js <infile.json> > outfile.json
+
+const filename = process.argv[2];
+const products = require(filename)
+
+const newObj = {};
+products.forEach(product => {
+    const productId = product.productId;
+    delete product.productId;
+    newObj[productId] = product;
+})
+
+console.log(JSON.stringify(newObj, null, 4));
